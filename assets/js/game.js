be32a244bc5370d06f4e80cb56c296d24dc3de4b
Dima <dmitrij.sazniov@gmail.com>
2024-05-22 14:40:52 +0000

    f: added eventlisteners for start button and answer options. added startGame function (checks if input is empty), if not continues to the game.

---
+++ assets/js/game.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Event listener for the Start Game button
+    document.getElementById("start-btn").addEventListener("click", startGame);
+
+    // Event Lisntener for all answer options
+    let options = document.getElementsByClassName("option");
+    for (let option of options) {
+        option.addEventListener("click", checkAnswer);
+    }
+})
+
+function startGame() {
+    // Username entered by user
+    let username = document.getElementById("username").value;
+
+    // Check if username input is empty
+    if (username === "") {
+        alert("Please enter your username");
+        return;
+    }
+
+    // Hides username input container, shows the game and displays user username.
+    document.getElementById("username-display").innerText = username;
+    document.getElementsByClassName("username-container")[0].style.display = "none";
+    document.getElementById("game-container").style.display = "block";
+
+    // Display question
+    displayQuestion();
+}
+
+function displayQuestion() {
+
+}
+
+function checkAnswer() {
+
+}
+
